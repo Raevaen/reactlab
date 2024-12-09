3e8fcf68d600c930607208d33c712560eb9e0e7b
Emanuele De Matteis <47746088+Raevaen@users.noreply.github.com>
2024-12-09 15:04:17 +0000

    update MyToken.ts

--- ignition/modules/MyToken.ts
+++ ignition/modules/MyToken.ts
@@ -1,9 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("MyToken", (m) => {
-  const myToken = m.contract("MyToken", [100]);
-
-  m.call(myToken, "launch", []);
+  const myToken = m.contract("MyToken", [1000000]);
 
   return { myToken };
 });
